3c64bdd55ff6fed679ffe7cf0143b5c46991ad7c
Takehiro Iyatomi <iyatomi@gmail.com>
2016-07-15 14:40:55 +0900

    define library interface

--- lib.cpp
+++ lib.cpp
@@ -1,5 +1,22 @@
 #include "lib.h"
 
-const char *hello() {
-	return "world";
+extern "C" {
+	extern shutup *shutup_new(const char *matcher, shutup_allocator *a) {
+		return NULL;
+	}
+	extern shutup *shutup_setup_alias(shutup *s, const char ***alias_groups) {
+		return s;
+	}
+	extern shutup *shutup_setup_mask(shutup *s, const char *mask) {
+		return s;
+	}
+	extern shutup *shutup_setup_word(shutup *s, const char *word) {
+		return s;
+	}
+	extern const char *shutup_should_block(shutup *s, const char *text, const shutup_block_config *cfg) {
+		return NULL;
+	}
+	const char *shutup_block(shutup *s, const char *text, const shutup_block_config *cfg) {
+		return text;
+	}
 }
--- lib.h
+++ lib.h
@@ -1,5 +1,22 @@
 #pragma once
 
+#include <stdio.h>
+
 extern "C" {
-	const char *hello();
+	typedef void *shutup;
+	typedef struct shutup_allocator {
+		void *(*malloc)(size_t);
+		void (*free)(void *);
+		void *(*realloc)(void *, size_t);
+	};
+	typedef struct shutup_block_config {
+		bool check_alias;
+		bool full_combination;
+	};
+	extern shutup *shutup_new(const char *matcher, shutup_allocator *a);
+	extern shutup *shutup_setup_alias(shutup *s, const char ***alias_groups);
+	extern shutup *shutup_setup_mask(shutup *s, const char *mask);
+	extern shutup *shutup_setup_word(shutup *s, const char *word);
+	extern const char *shutup_should_block(shutup *s, const char *text, const shutup_block_config *cfg);
+	const char *shutup_block(shutup *s, const char *text, const shutup_block_config *cfg);
 }
